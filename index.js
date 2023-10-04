29bcb0fc2d14ef272344ba996097e78e5d2dd884
Marozva <isabokefrankline@gmail.com>
2023-10-04 13:49:37 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
+function reverseString(word){
+  // create an array of word by the split method, reverse it and then return the joined string
+  return word.split('').reverse('').join('')
+}
+
 function isPalindrome(word) {
-  // Write your algorithm here
+  // algorithm here
+  const reverseWord = reverseString(word);
+  return word === reverseWord
+   
 }
 
 /* 
@@ -7,7 +15,7 @@ function isPalindrome(word) {
 */
 
 /*
-  Add written explanation of your solution here
+  We are supposed to creat a function that takes in a string and return it reversed
 */
 
 // You can run `node index.js` to view these console logs
@@ -15,7 +23,18 @@ if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
+  console.log("");
 
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("ab"));
   console.log("");
 
   console.log("Expecting: false");
